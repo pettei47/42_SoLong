5fd8889157c618e9e36e83cbdecc223590c1e42b
pettei47 <kitagawa.teppei.33c@gmail.com>
2021-08-16 22:31:02 +0900

    update: mainの最後にfree_longを追加

--- solong_main.c
+++ solong_main.c
@@ -6,7 +6,7 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 16:18:44 by teppei            #+#    #+#             */
-/*   Updated: 2021/08/16 01:02:57 by teppei           ###   ########.fr       */
+/*   Updated: 2021/08/16 22:30:15 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	main(int ac, char **av)
 
 	sl_init_long(&l);
 	sl_check_ber(ac, av, &l);
-	printf("map_y:%ld\n", l.map_y);
 	for(int i = 0; i < l.map_y; i++)
 		printf("[%d]%s\n", i, l.map[i]);
+	sl_free_long(&l);
 	return (0);
 }
